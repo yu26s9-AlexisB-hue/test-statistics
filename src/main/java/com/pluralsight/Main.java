d1c37ecc29de6eaec1637a17940423e0c643ab6c
Alexis Barrett <happynessb09@gmail.com>
2026-04-16 21:06:16 -0400

    finally figured it out

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -8,9 +8,15 @@ public class Main {
                 50,47,89,97,100,72,34,66,91,84
         };
 
+        int sum = 0;
         Arrays.sort(testScores);
         for(int score : testScores){
-            System.out.println(score);
+            sum += score;
         }
+        int average = sum / (testScores.length);
+        System.out.println("The average test score: " + average);
+        System.out.println("The highscore is: " + testScores[9]);
+        System.out.println("The lowest score is: "+ testScores[0]);
+
     }
 }
